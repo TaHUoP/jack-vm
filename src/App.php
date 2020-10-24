a26d02cdfb14e9eaedae647b626eff2813fcec85
vazghen <malhasianv@gmail.com>
2020-10-24 23:53:44 +0300

    initial commit

---
+++ src/App.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace TaHUoP;
+
+use Exception;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\SingleCommandApplication;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class App extends SingleCommandApplication
+{
+    private Parser $parser;
+
+    public function __construct(Parser $parser)
+    {
+        parent::__construct();
+        $this
+            ->addArgument('inputFilePath', InputArgument::REQUIRED, 'Path to .vm file')
+            ->addArgument('outputFilePath', InputArgument::OPTIONAL, 'Path to .asm file')
+            ->addArgument('memoryLimit', InputArgument::OPTIONAL, 'PHP memory limit. Unlimited by default')
+            ->setCode([$this, 'main']);
+        $this->parser = $parser;
+    }
+
+    public function main(InputInterface $input, OutputInterface $output): void
+    {
+        try {
+            ini_set('memory_limit', $input->getArgument('memoryLimit') ?? -1);
+
+            $inputFilePath = $input->getArgument('inputFilePath');
+            $outputFileContent = $this->parser->parseFile($inputFilePath);
+
+            $outputFilePath = $input->getArgument('outputFilePath') ?? $this->getOutputFilePath($inputFilePath);
+            if (file_put_contents($outputFilePath, $outputFileContent)) {
+                $output->writeln("File $outputFilePath was successfully built.");
+            } else {
+                $output->writeln("<fg=red>Unable to write to file $outputFilePath.</>");
+            }
+        } catch (Exception $e) {
+            $output->writeln('<fg=red>' . $e->getMessage() . '</>');
+        }
+    }
+
+    private function getOutputFilePath(string $inputFilePath): string
+    {
+        $pathInfo = pathinfo($inputFilePath);
+        return $pathInfo['dirname'] . DIRECTORY_SEPARATOR . $pathInfo['filename'] . '.asm';
+    }
+}
---
+++ src/Parser.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace TaHUoP;
+
+
+class Parser
+{
+    public function parseFile(string $filePath): string
+    {
+        return '';
+    }
+}
